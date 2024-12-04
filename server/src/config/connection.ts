ab3812d85cbc76efec4c538e246f3fed0010d144
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-12-04 18:14:51 -0500

    updated mongo string

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
